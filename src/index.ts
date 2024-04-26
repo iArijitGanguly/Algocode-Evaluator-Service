6c1205943b858f09a0f8bf579eb161f2a3f4395d
iArijitGanguly <arijeetGanguly63@gmail.com>
2024-04-26 18:23:11 +0530

    Basuc api set up is done

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 
 import serverConfig from './configs/serverConfig';
+import apiRouter from './routers';
 
 const app = express();
+
+app.use('/api', apiRouter);
 
 app.listen(serverConfig.PORT, () => {
     console.log(`Server started at PORT: ${serverConfig.PORT}`);
