39df91365600831ef43d7c668e4ea49d1637e6a5
iArijitGanguly <arijeetGanguly63@gmail.com>
2024-04-27 23:06:21 +0530

    Added a dashboard for queues using bullboard

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,28 @@
+import { createBullBoard, } from '@bull-board/api';
+import { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
+import { ExpressAdapter } from '@bull-board/express';
 import express from 'express';
 
 import logger from './configs/loggerConfig';
 import serverConfig from './configs/serverConfig';
 import sampleQueueProducer from './producers/sampleQueueProducer';
+import sampleQueue from './queues/sampleQueue';
 import apiRouter from './routers';
 import SampleWorker from './workers/SampleWorker';
+
+const serverAdapter = new ExpressAdapter();
+serverAdapter.setBasePath('/admin/queues');
+
+createBullBoard({
+    queues: [new BullMQAdapter(sampleQueue)],
+    serverAdapter: serverAdapter
+});
 
 const app = express();
 const { PORT } = serverConfig;
 
 app.use('/api', apiRouter);
+app.use('/admin/queues', serverAdapter.getRouter());
 
 app.listen(PORT, () => {
     logger.info(`Server started at PORT: ${PORT}`);
