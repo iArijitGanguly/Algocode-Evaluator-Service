9d4833bd12ef853c7e432d3f925d05394c092d49
iArijitGanguly <arijeetGanguly63@gmail.com>
2024-05-03 01:08:17 +0530

    Fix: Segregate the bullboard logic from the index.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,17 @@
-import { createBullBoard, } from '@bull-board/api';
-import { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
-import { ExpressAdapter } from '@bull-board/express';
 import express from 'express';
 
+import bullboardAdapter from './configs/bullboardConfig';
 import logger from './configs/loggerConfig';
 import serverConfig from './configs/serverConfig';
 import sampleQueueProducer from './producers/sampleQueueProducer';
-import sampleQueue from './queues/sampleQueue';
 import apiRouter from './routers';
 import SampleWorker from './workers/SampleWorker';
-
-const serverAdapter = new ExpressAdapter();
-serverAdapter.setBasePath('/admin/queues');
-
-createBullBoard({
-    queues: [new BullMQAdapter(sampleQueue)],
-    serverAdapter: serverAdapter
-});
 
 const app = express();
 const { PORT } = serverConfig;
 
 app.use('/api', apiRouter);
-app.use('/admin/queues', serverAdapter.getRouter());
+app.use('/bullboard', bullboardAdapter.getRouter());
 
 app.listen(PORT, () => {
     logger.info(`Server started at PORT: ${PORT}`);
